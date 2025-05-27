9946f40102d491008c66fe6c658a6fd6b6f7df99
eckofox1981 <eckofox@pm.me>
2025-05-27 14:03:07 +0200

    GROUP SESSIONS: fixed small error in texts, tried new color scheme

--- javascript/pages/headerAndFooter.js
+++ javascript/pages/headerAndFooter.js
@@ -18,7 +18,7 @@ function headerMaker () {
     linkHome.href = "index.html";
     bulletinBoard.innerHTML = '<a href="bulletin-board.htlm">Bulletin Board</a>';
     friends.innerHTML = '<a href="friends.htlm">Friends</a>';
-    messages.innerHTML = '<a href="messages.htlm">Bulletin Board</a>';
+    messages.innerHTML = '<a href="messages.htlm">Messages</a>';
     account.innerHTML = '<a href="account.htlm">Account</a>';
     hamburger.innerHTML = `
                 <svg
@@ -32,7 +32,7 @@ function headerMaker () {
             fill-rule="evenodd"
             clip-rule="evenodd"
             d="M4 5C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H4ZM7 12C7 11.4477 7.44772 11 8 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H8C7.44772 13 7 12.5523 7 12ZM13 18C13 17.4477 13.4477 17 14 17H20C20.5523 17 21 17.4477 21 18C21 18.5523 20.5523 19 20 19H14C13.4477 19 13 18.5523 13 18Z"
-            fill="#FF9F1C"
+            fill="#F1C40F"
           />
         </svg>
                 `;
@@ -114,8 +114,8 @@ function headerMaker () {
                     M588.69405,197.42391 L617.92412,184.6037
                     M620.48816,185.62932 L523.56742,191.78302
                 "
-                fill="#FF9F1C"
-                stroke="#FF9F1C"
+                fill="#F1C40F"
+                stroke="#F1C40F"
                 fill-rule="evenodd"
                 clip-rule="evenodd"
                 />
