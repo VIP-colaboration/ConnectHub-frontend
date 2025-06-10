e329517f6e451bb5daf7bdbde775a4c50fa78cfe
eckofox1981 <eckofox@pm.me>
2025-06-10 12:26:52 +0200

    fixed css causing dropdown menu to appear behind other divs

--- javascript/pages/account.js
+++ javascript/pages/account.js
@@ -17,10 +17,17 @@ const commentCounter = document.getElementById("commentCounter");
 const likedCtrl = document.getElementById("likedCtrl");
 const likedCounter = document.getElementById("likeCounter");
 
+//ITEMS TO SHOW
+const itemsToShow = document.getElementById("itemsToShow");
+const friendsContainer = document.createElement("section");
+
+
+friendsContainer.classList = "friends-container";
+
 userEmail.addEventListener("mouseenter", () => {
     //test code
     showToast("click to change your email address");
-})
+});
 
 visibilityMode.addEventListener("click", () => {
     //test code
@@ -31,3 +38,35 @@ visibilityMode.addEventListener("click", () => {
         console.log("visible");
     }
 });
+
+const placeholderFriends = [
+  { name: "Jen", avatar: "https://xsgames.co/randomusers/avatar.php?g=female" },
+  { name: "Super Cat", avatar: "https://xsgames.co/randomusers/avatar.php?g=male" },
+  { name: "Marie Taylor Greene", avatar: "https://xsgames.co/randomusers/avatar.php?g=female" },
+  { name: "Bob", avatar: "https://xsgames.co/randomusers/avatar.php?g=male" },
+  { name: "Osama", avatar: "https://xsgames.co/randomusers/avatar.php?g=male" }
+];
+
+showFriends();
+
+function showFriends () {
+    
+    
+
+    for (let friend of placeholderFriends) {
+        const singleFriend = document.createElement("div");
+        const friendAvatar = document.createElement("img");
+        const friendName = document.createElement("h2");
+
+        singleFriend.classList = "user-logo-and-name";
+        singleFriend.append(friendAvatar, friendName);
+
+        friendAvatar.src = friend.avatar;
+        friendName.textContent = friend.name;
+
+        friendsContainer.appendChild(singleFriend);
+    }
+
+    itemsToShow.innerHTML = "";
+    itemsToShow.appendChild(friendsContainer);
+}
