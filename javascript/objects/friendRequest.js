395f52db47f6df3e0f691ceb669419efec103496
eckofox1981 <eckofox@pm.me>
2025-07-28 00:35:02 +0200

    added FriendRequest.js, exported to friends.js. Worked on a displayable friendRequest. Functionnaly working but needs css work done to it.

---
+++ javascript/objects/friendRequest.js
@@ -0,0 +1,45 @@
+export class FriendRequest {
+    constructor (id, requesterID, requesterUsername, requestedID, requestedUsername, status, message, created, updated) {
+        this.id = id;
+        this.requesterID = requesterID;
+        this.requesterUsername = requesterUsername;
+        this.requestedID = requestedID;
+        this.requestedUsername = requestedUsername;
+        this.status = status;
+        this.message = message;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    requestElementsForRequested() {
+        const requestDiv = document.createElement("div");
+        const requesterUsername = document.createElement("h2");
+        const status = document.createElement("p");
+        const message = document.createElement("p");
+        const created = document.createElement("i");
+        const updated = document.createElement("i");
+        const acceptBtn = document.createElement("button");
+        const declineBtn = document.createElement("button");
+
+        acceptBtn.className = "primaryBtn";
+        declineBtn.className = "secondaryBtn";
+
+        acceptBtn.textContent = "Accept";
+        declineBtn.textContent = "Decline";
+
+        requesterUsername.textContent = this.requesterUsername;
+        status.textContent = this.status;
+        message.textContent = this.message;
+        created.textContent = this.created;
+        updated.textContent = this.updated;
+
+        requestDiv.append(requesterUsername, message, created,status, acceptBtn, declineBtn);
+
+        return requestDiv;
+
+    
+    }
+
+
+}
+
--- javascript/pages/friends.js
+++ javascript/pages/friends.js
@@ -1,15 +1,20 @@
 import { setToken, removeToken, getToken, saveProfilePicture, getProfilePicture, removeProfilePicture, getUserID, getUsername } from "../objects/token.js";
 import { showToast } from "./main.js";
 import { User } from "../objects/user.js";
+import { FriendRequest } from "../objects/friendRequest.js";
 
 //FIXED ELEMENTS (always diplayed/included)
 const userImage = document.getElementById("userImage");
 const username = document.getElementById("username");
 const userID =document.getElementById("userID");
 const flexibleSection = document.getElementById("flexibleSection");
+const friendRequestField = document.getElementById("friendRequestField");
+const requestButton = document.getElementById("searchByUserIDBtn");
 
 //FOR FLEXIBLE SECTION
 const currentFriendRequests = document.createElement("h2");
+
+requestButton.addEventListener("click", sendFriendRequest);
 
 setUpUserDetail();
 checkForFriendRequest();
@@ -23,21 +28,17 @@ function setUpUserDetail() {
     } else {
         console.error("User image not found in local storage");
     }
-
-
-    console.log(getUsername());
-    console.log(getUserID());
     
-    
-
     username.textContent= getUsername();
     userID.textContent = getUserID();
 }
 
 async function checkForFriendRequest() {
     let message;
+    
     try {
-        const response = await fetch("http://localhost:8080/get-pending-friend-requests", {
+        //fetch request
+        const response = await fetch(`http://localhost:8080/get-pending-friend-requests`, {
             method: "GET",
             headers: {
                 "Authorization": getToken(),
@@ -46,7 +47,6 @@ async function checkForFriendRequest() {
         });
 
         if (!response.ok) {
-            console.log("error at fetching friend requests")
             message = await response.text();
             throw new Error(message);
         }
@@ -58,10 +58,62 @@ async function checkForFriendRequest() {
             flexibleSection.append(currentFriendRequests);
         } else {
             //TODO: SETUP DISPLAY OF FRIEND REQUESTS
-            console.log(friendRequestList);
+            for (let friendRequ of friendRequestList){
+                const friendRequest = new FriendRequest (
+                    friendRequ.id,
+                    friendRequ.requesterID,
+                    friendRequ.requesterUsername,
+                    friendRequ.requestedID,
+                    friendRequ.requestedUsername,
+                    friendRequ.status,
+                    friendRequ.message,
+                    friendRequ.created,
+                    friendRequ.updated,
+                )
+                const friendRequestTitle = document.createElement("h1");
+                friendRequestTitle.textContent = "Friend Requests";
+                flexibleSection.append(friendRequestTitle, friendRequest.requestElementsForRequested());
+            }
         }
         
     } catch (error) {
         console.log(error.message)
+    }
+}
+
+
+async function sendFriendRequest() {
+    let message;
+    if (!friendRequestField.value) {
+        showToast("You have to fill in the user ID to send the request.")
+        return;
+    }
+
+    try {
+        //building parametes to be sent
+        const params = new URLSearchParams ({
+            requestedId : friendRequestField.value
+        });
+        console.log(params);
+        
+
+        const response = await fetch(`http://localhost:8080/send-friend-request?${params}`, {
+            method: "POST",
+            headers: {
+                "Authorization" : getToken(),
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
+        });
+
+        if (!response.ok) {
+            message = await response.text();
+            throw new Error(message);
+        }
+
+        message = await response.text();
+        showToast(message);
+    } catch (error) {
+        showToast(error.message);
+        console.error(message);
     }
 }
