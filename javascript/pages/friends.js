750a3a712b8925f2c4308550ea3a8a492d7eb8b1
eckofox1981 <eckofox@pm.me>
2025-07-27 22:57:24 +0200

    added search fields and labels to friends.html. Added 'checkForFriendRequest()' in friends.js, as this commit only setup for empty return from API.

--- javascript/pages/friends.js
+++ javascript/pages/friends.js
@@ -2,11 +2,17 @@ import { setToken, removeToken, getToken, saveProfilePicture, getProfilePicture,
 import { showToast } from "./main.js";
 import { User } from "../objects/user.js";
 
+//FIXED ELEMENTS (always diplayed/included)
 const userImage = document.getElementById("userImage");
 const username = document.getElementById("username");
 const userID =document.getElementById("userID");
+const flexibleSection = document.getElementById("flexibleSection");
+
+//FOR FLEXIBLE SECTION
+const currentFriendRequests = document.createElement("h2");
 
 setUpUserDetail();
+checkForFriendRequest();
 
 function setUpUserDetail() {
     const localStorageImg = localStorage.getItem("profilePicture");
@@ -26,4 +32,36 @@ function setUpUserDetail() {
 
     username.textContent= getUsername();
     userID.textContent = getUserID();
+}
+
+async function checkForFriendRequest() {
+    let message;
+    try {
+        const response = await fetch("http://localhost:8080/get-pending-friend-requests", {
+            method: "GET",
+            headers: {
+                "Authorization": getToken(),
+                "Content-type" : "application/json",
+            },
+        });
+
+        if (!response.ok) {
+            console.log("error at fetching friend requests")
+            message = await response.text();
+            throw new Error(message);
+        }
+
+        const friendRequestList = await response.json();
+        if (friendRequestList.length === 0) {
+            console.log("empty list");
+            currentFriendRequests.textContent = "No friend request received";
+            flexibleSection.append(currentFriendRequests);
+        } else {
+            //TODO: SETUP DISPLAY OF FRIEND REQUESTS
+            console.log(friendRequestList);
+        }
+        
+    } catch (error) {
+        console.log(error.message)
+    }
 }
