886d7e615ece0c0f358d362a851666aac0747f22
eckofox1981 <eckofox@pm.me>
2025-07-28 11:38:30 +0200

    implemented accept friendRequest in friendRequest.js

--- javascript/objects/friendRequest.js
+++ javascript/objects/friendRequest.js
@@ -1,3 +1,6 @@
+import { getToken } from "../objects/token.js";
+import { showToast } from "../pages/main.js";
+
 export class FriendRequest {
     constructor (id, requesterID, requesterUsername, requestedID, requestedUsername, status, message, created, updated) {
         this.id = id;
@@ -38,13 +41,35 @@ export class FriendRequest {
         created.textContent = this.created;
         updated.textContent = this.updated;
 
+        acceptBtn.addEventListener("click", ()=> acceptfriendRequest(requestDiv, acceptBtn.id));
+
         requestDiv.append(requesterUsername, message, created,status, acceptBtn, declineBtn);
 
         return requestDiv;
-
-    
     }
-
-
 }
 
+async function acceptfriendRequest(requestDiv, requestID) {
+    let message;
+
+    try {
+        const response = await fetch(`http://localhost:8080/accept-friend-request/${requestID}`, {
+            method: "POST",
+            headers: {
+                "Authorization": getToken(),
+            },
+        });
+
+        if (!response.ok) {
+            message = await response.text();
+            throw new Error(message);
+        }
+
+        message = await response.text();
+        showToast(message);
+        requestDiv.parentNode.removeChild(requestDiv);
+
+    } catch (error) {
+
+    }
+}
